5ae6e4bc97662b613e0b1e70cda1ab50221d2a7a
Mingkui Liu <mingkui.liu@hulu.com>
2013-03-31 20:50:31 -0700

    delete sub install.sh

--- bash/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-dot_path=$( cd "$( dirname $( dirname "${BASH_SOURCE[0]}" ) )" && pwd )
-echo "Append the bash to ~/.bashrc"
-echo 'dot_path='$dot_path >> ~/.bashrc
-cat $dot_path/bash/.bashrc >> ~/.bashrc
