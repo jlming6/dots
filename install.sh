6ed4f6bc9117ac2c112c9314cbc3c1b7c35a2601
Mingkui Liu <mingkui.liu@elsreco-offline05.(none)>
2013-06-02 03:57:54 -0700

    Improve the reco dots install bash script

--- install.sh
+++ install.sh
@@ -2,47 +2,62 @@
 
 dot_path=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-printf '#### Bash ####\n'
-echo "Append the bash/.bashrc to ~/.bashrc"
-echo 'dot_path='$dot_path >> ~/.bashrc
-cat $dot_path/bash/.bashrc >> ~/.bashrc
-printf '\n'
+echo -ne "Install advanced bashrc(Now improve git command and some alias? (y|n): "
+read ans
+case "$ans" 
+    in y|Y|yes|YES|yes)
+        printf '#### Bash ####\n'
+        echo "Append the bash/.bashrc to ~/.bashrc"
+        echo 'dot_path='$dot_path >> ~/.bashrc
+        cat $dot_path/bash/.bashrc >> ~/.bashrc
+        printf '\n';;
+esac
 
-printf '#### Git ####\n'
-user_name=$(git config --get user.name)
-user_email=$(git config --get user.email)
+echo -ne "Install advanced git config? (y|n): "
+read ans
+case "$ans" 
+    in y|Y|yes|YES|yes)
+        printf '#### Git ####\n'
+        user_name=$(git config --get user.name)
+        user_email=$(git config --get user.email)
 
-echo "Git User Name: $user_name"
-echo "Git User Email: $user_email"
+        echo "Git User Name: $user_name"
+        echo "Git User Email: $user_email"
 
-sed -e "s/name =/name = $user_name/" -e "s/email =/email = $user_email/" $dot_path/git/linux/.gitconfig > $dot_path/.gitconfig.bak
+        sed -e "s/name =/name = $user_name/" -e "s/email =/email = $user_email/" $dot_path/git/linux/.gitconfig > $dot_path/.gitconfig.bak
 
-if [ -f ~/.gitconfig ]
-then
-    echo "Backup old gitconfig file"
-    cp ~/.gitconfig ~/.gitconfig_bak
-fi
+        if [ -f ~/.gitconfig ]
+        then
+            echo "Backup old gitconfig file"
+            cp ~/.gitconfig ~/.gitconfig_bak
+        fi
 
-echo "Copy .gitconfig file to ~/.gitconfig"
-cp $dot_path/.gitconfig.bak ~/.gitconfig
-printf '\n'
+        echo "Copy .gitconfig file to ~/.gitconfig"
+        cp $dot_path/.gitconfig.bak ~/.gitconfig
+        printf '\n';;
+esac
 
-printf '#### Vim ####\n'
-if [ -f ~/.vimrc ]
-then
-    echo "Backup old vimrc file"
-    cp ~/.vimrc ~/.vimrc_bak
-fi
+echo -ne "Install advanced vimrc? (y|n): "
+read ans
+case "$ans" 
+    in y|Y|yes|YES|yes)
+        printf '#### Vim ####\n'
+        if [ -f ~/.vimrc ]
+        then
+        echo "Backup old vimrc file"
+        cp ~/.vimrc ~/.vimrc_bak
+        fi
 
-echo "Copy vimrc file"
-cp $dot_path/vim/.vimrc ~/.vimrc
+        echo "Copy vimrc file"
+        cp $dot_path/vim/.vimrc ~/.vimrc
 
-if [ ! -d ~/.vim/bundle/vundle ]
-then
-    echo "Clone the vundle code"
-    git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-fi
+        if [ ! -d ~/.vim/bundle/vundle ]
+        then
+            echo "Clone the vundle code"
+            git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+        fi
 
-echo "Install ctags"
-echo "y" | sudo yum install ctags > /dev/null
-printf '\n'
+        echo "Install ctags"
+        echo "y" | sudo yum install ctags > /dev/null
+        printf '\n';;
+esac
